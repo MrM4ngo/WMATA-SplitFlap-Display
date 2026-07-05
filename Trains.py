aca7ed66e3883e54b0b00bfe86b6a8e3485d098e
David Bui <109239230+MrM4ngo@users.noreply.github.com>
2026-07-04 20:10:30 -0400

    added abv to train names

--- Trains.py
+++ Trains.py
@@ -3,6 +3,7 @@ import requests
 import csv
 from dotenv import load_dotenv
 import Variables
+import time
 
 load_dotenv()
 api_key = os.getenv("API_KEY")
@@ -35,8 +36,8 @@ def Trains(station_code,Threshold):
                 return int(min_value)
 
         Size = len(trains)
-        print(trains)
-        print(Size)
+        # print(trains)
+        # print(Size)
 
         TrainList = []
 
@@ -53,33 +54,9 @@ def Trains(station_code,Threshold):
     if predictions:
         next_trains = get_next_viable_trains(predictions, Threshold=Threshold)
     return next_trains
-
-
-def get_all_stations():
-    url = "https://api.wmata.com/Rail.svc/json/jStations"
-    headers = {"api_key": api_key}
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        return response.json()
-    else:
-        print(f"Error fetching stations: {response.status_code}")
-        return None
     
-def save_stations_to_csv(station_data, filename="wmata_stations.csv"):
-    stations = station_data.get("Stations", [])
-    
-    if not stations:
-        print("No station data found to save.")
-        return
-
-    headers = stations[0].keys()
-
-    with open(filename, mode="w", newline="", encoding="utf-8") as file:
-        writer = csv.DictWriter(file, fieldnames=headers)
-        writer.writeheader()
-        writer.writerows(stations)
-        
-    print(f"Successfully downloaded and saved {len(stations)} stations to '{filename}'!")
 
 if __name__ == "__main__":
-    print(Trains(Variables.TrainStationCode, Variables.MinuteThreshold))
+    while True:
+        print(Trains(Variables.TrainStationCode, Variables.MinuteThreshold))
+        time.sleep(Variables.TrainRefreshTime)
